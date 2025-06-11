8a7c3a06c0c86a08c32003a5b14330e42a226eed
oxymoron0 <seyoumoon@gmail.com>
2025-06-11 22:17:00 +0900

    refactor(filter): rename filter package and update filtering function

--- validFilter/filter.go
+++ validFilter/filter.go
@@ -1,4 +1,4 @@
-package filter
+package validFilter
 
 import (
 	"context"
@@ -20,16 +20,17 @@ func shouldDrop(detectionInfo interface{}) bool {
 	return false
 }
 
-func filter(ctx goka.Context, msg interface{}) {
+func validFilter(ctx goka.Context, msg interface{}) {
 	originMsg, ok := msg.(messaging.OriginMessage)
 	if !ok {
 		return
 	}
-
+	// fmt.Println("originMsg", originMsg)
+	// originMsg {6d869d24-9fea-4f9a-978b-c85cc93b9693 2025-06-11 19:55:23.028 +0000 UTC 2025-06-11 19:55:22.73788723 +0000 UTC 1920 1080 fire detection {AST_DS7.1_TEST [map[classID:0 confidence:0.99951171875 height:38 left:1009 objectID:0 top:216 width:28]]}}
 	if shouldDrop(originMsg.InferenceResult.DetectionInfo) {
 		return
 	}
-	ctx.Emit(messaging.ValidInferenceStream, originMsg.InferenceResult.ID, originMsg)
+	ctx.Emit(messaging.ValidInferenceStream, originMsg.InferenceType, originMsg)
 }
 
 func PrepareTopics(brokers []string, config *sarama.Config) {
@@ -40,7 +41,7 @@ func PrepareTopics(brokers []string, config *sarama.Config) {
 func Run(ctx context.Context, brokers []string) func() error {
 	return func() error {
 		g := goka.DefineGroup(group,
-			goka.Input(messaging.OriginTopic, new(messaging.OriginMessageCodec), filter),
+			goka.Input(messaging.OriginTopic, new(messaging.OriginMessageCodec), validFilter),
 			goka.Output(messaging.ValidInferenceStream, new(messaging.OriginMessageCodec)),
 		)
 		p, err := goka.NewProcessor(brokers, g)
