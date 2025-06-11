522002a4deb38e21b7594d9357cfc7fe8b7421ee
oxymoron0 <seyoumoon@gmail.com>
2025-06-12 00:44:32 +0900

    feat(services): implement Sample HTTP handler for message feeding from view

--- services/service.go
+++ services/service.go
@@ -11,6 +11,24 @@ import (
 	"github.com/lovoo/goka/examples/3-messaging/collector"
 	messaging "github.com/oxymoron0/simple-goka-streaming-pipeline"
 )
+
+// View, Emitter 생성
+func Run(brokers []string, stream goka.Stream) {
+	// View 생성
+	view, err := goka.NewView(brokers, collector.Table, new(collector.MessageListCodec))
+	if err != nil {
+		panic(err)
+	}
+	go view.Run(context.Background())
+
+	// Router 생성
+	router := mux.NewRouter()
+	router.HandleFunc("/{user}/feed", feed(view)).Methods("GET")
+
+	log.Printf("Listen port 8080")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
 // View 사용하여 메세지 피드 HTTP로 출력(핸들러 함수 return)
 func feed(view *goka.View) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
